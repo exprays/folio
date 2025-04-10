d6ef8eae0739e05bf40ec36b1a5d418562d583c0
Surya <suryakant.subudhi@outlook.com>
2025-04-10 22:38:14 +0530

    fix

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -229,7 +229,7 @@ export const DATA = {
       ],
       image: "",
       video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
+        "https://exprays.thestarsociety.tech/videos/yarn.mp4",
     },
   ],
   hackathons: [
